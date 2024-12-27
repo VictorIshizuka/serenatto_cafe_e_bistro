b13a4f0a40bdd4fc418c3022475784118c0cc564
Victor Rosario Ishizuka <victorishizuka131@gmail.com>
2024-12-27 18:38:27 -0400

    conectando banco de dados

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php
+
+require "src/conexao-bd.php";
+
+$sql1 = "SELECT * FROM produtos WHERE tipo = 'Café'";
+$statement = $pdo->query($sql1);
+$produtosCafe = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$sql2 = "SELECT * FROM produtos WHERE tipo = 'Almoço'";
+$statement = $pdo->query($sql2);
+$produtosAlmoco = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="img/icone-serenatto.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>Serenatto - Cardápio</title>
+</head>
+
+<body>
+    <main>
+        <section class="container-banner">
+            <div class="container-texto-banner">
+                <img src="img/logo-serenatto.png" class="logo" alt="logo-serenatto">
+            </div>
+        </section>
+        <h2>Cardápio Digital</h2>
+        <section class="container-cafe-manha">
+            <div class="container-cafe-manha-titulo">
+                <h3>Opções para o Café</h3>
+                <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
+            </div>
+            <div class="container-cafe-manha-produtos">
+                <?php foreach ($produtosCafe as $data): ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= "img/" . $data['imagem'] ?>">
+                        </div>
+                        <p><?= $data['nome'] ?></p>
+                        <p><?= $data['descricao'] ?></p>
+                        <p> <?= "R$ " . $data['preco'] ?></p>
+                    </div>
+                <?php endforeach; ?>
+                <!-- <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="img/cafe-com-leite.jpg">
+                    </div>
+                    <p>Café com Leite</p>
+                    <p>A harmonia perfeita do café e do leite, uma experiência reconfortante</p>
+                    <p>R$ 2.00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="img/cappuccino.jpg">
+                    </div>
+                    <p>Cappuccino</p>
+                    <p>Café suave, leite cremoso e uma pitada de sabor adocicado</p>
+                    <p>R$ 7.00</p>
+                </div>
+                <div class="container-produto">
+                    <div class="container-foto">
+                        <img src="img/cafe-gelado.jpg">
+                    </div>
+                    <p>Café Gelado</p>
+                    <p>Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.</p>
+                    <p>R$ 3.00</p>
+                </div> -->
+            </div>
+        </section>
+        <section class="container-almoco">
+            <div class="container-almoco-titulo">
+                <h3>Opções para o Almoço</h3>
+                <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
+            </div>
+            <div class="container-almoco-produtos">
+                <?php foreach ($produtosAlmoco as $data) : ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= "img/" . $data['imagem'] ?>"> 
+                        </div>
+                        <p><?= $data['nome'] ?></p>
+                        <p><?= $data['descricao'] ?></p>
+                        <p> <?= "R$ " . $data['preco'] ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
